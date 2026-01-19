5e30c8acdf2747c806f91fb96ab6e3c5452fe12d
codingbaraGo <hyunchang52@gmail.com>
2026-01-19 18:42:24 +0900

    Initial commit for backend

---
+++ backend/src/main/java/com/genesis/unipocket/UnipocketApplication.java
@@ -0,0 +1,13 @@
+package com.genesis.unipocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnipocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnipocketApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/genesis/unipocket/TestUnipocketApplication.java
@@ -0,0 +1,11 @@
+package com.genesis.unipocket;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestUnipocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(UnipocketApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}

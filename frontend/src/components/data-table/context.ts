ca9dbe3f870f45a4d09b39347cd45abd19745253
Mingyu Seong <minngyuseong@gmail.com>
2026-01-22 17:37:09 +0900

    chore: dispatch 타입 수정

--- frontend/src/components/data-table/context.ts
+++ frontend/src/components/data-table/context.ts
@@ -1,11 +1,11 @@
-import type { Table } from "@tanstack/react-table";
-import { createContext, useContext, type ActionDispatch } from "react";
-import type { TableUIAction, TableUIState } from "./type";
+import type { Table } from '@tanstack/react-table';
+import { createContext, useContext } from 'react';
+import type { TableUIAction, TableUIState } from './type';
 
 interface DataTableContextType<TData> {
   table: Table<TData>;
   tableState: TableUIState;
-  dispatch: ActionDispatch<[action: TableUIAction]>;
+  dispatch: React.Dispatch<TableUIAction>;
 }
 
 export const DataTableContext = createContext<DataTableContextType<any> | null>(
@@ -15,7 +15,7 @@ export const DataTableContext = createContext<DataTableContextType<any> | null>(
 export const useDataTable = () => {
   const context = useContext(DataTableContext);
   if (!context) {
-    throw new Error("useDataTable must be used within a DataTableProvider");
+    throw new Error('useDataTable must be used within a DataTableProvider');
   }
   return context;
 };

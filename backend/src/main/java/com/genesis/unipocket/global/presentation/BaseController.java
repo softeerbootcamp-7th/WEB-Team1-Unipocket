ed3cdd01cf524245e07ccf5c5a9cf0f24da16230
Chanwoo Kim <kcw2205@naver.com>
2026-01-23 16:32:22 +0900

    feat(global): 헬스체크 컨트롤러 개발, 기본 엔티티 설정
    * feat(global): JPA용 공용 엔티티 추가
    
    - 기초적인 엔티티를 만들었습니다.
    - 해당 엔티티를 상속하여 엔티티를 만들면, 생성 시점과 수정 시점에 대한 정보를 자동으로 매핑하며 저장합니다.
    
    * feat(global): 헬스체크 컨트롤러 추가
    
    - 기본적으로 헬스체크를 할 수 있는 컨트롤러를 추가했습니다.
    - GET /health-check 엔트리포인트에 대해 문자열 "OK" 를 반환합니다.
    
    * fix(global): `EnableJpaAuditing` 어노테이션 적용
    
    @EntityListeners(AuditingEntityListener.class)를 사용하려면 애플리케이션에 @EnableJpaAuditing 어노테이션을 추가해야 합니다.
    
     그렇지 않으면 createdAt과 updatedAt 필드가 자동으로 채워지지 않습니다.
    
    * refactor(global): `BaseController` 내 불필요한 코드 개선
    
    - `RestController` 로 변경하여 코드 개선
    
    * feat(global): `BaseEntity` 에서 updatedAt 필드에도 nullable = false 제약조건을 추가
    
    #76

---
+++ backend/src/main/java/com/genesis/unipocket/global/presentation/BaseController.java
@@ -0,0 +1,20 @@
+package com.genesis.unipocket.global.presentation;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <b> 기본 컨트롤러 </b>
+ * <p>
+ * 헬스체크 역할 수행
+ */
+@RestController
+public class BaseController {
+
+    @GetMapping("/health-check")
+    public String healthCheck() {
+        return "OK";
+    }
+}

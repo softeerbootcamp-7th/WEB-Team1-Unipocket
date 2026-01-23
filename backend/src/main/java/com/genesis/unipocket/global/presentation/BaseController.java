804bf83a84af63a9057e28efeb02d364869df424
blu3fishez <kcw2205@naver.com>
2026-01-23 13:51:25 +0900

    refactor(global): `BaseController` 내 불필요한 코드 개선
    - `RestController` 로 변경하여 코드 개선

--- backend/src/main/java/com/genesis/unipocket/global/presentation/BaseController.java
+++ backend/src/main/java/com/genesis/unipocket/global/presentation/BaseController.java
@@ -3,17 +3,17 @@ package com.genesis.unipocket.global.presentation;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * <b> 기본 컨트롤러 </b>
  * <p>
  * 헬스체크 역할 수행
  */
-@Controller
+@RestController
 public class BaseController {
 
     @GetMapping("/health-check")
-    @ResponseBody
     public String healthCheck() {
         return "OK";
     }

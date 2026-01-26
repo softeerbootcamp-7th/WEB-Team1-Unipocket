28fa1c80b2f7cb2d55aca7c73f40e56e3d061a29
1jiwoo27 <chloey1227@gmail.com>
2026-01-26 17:06:57 +0900

    chore: 카테고리 스타일에서 label 제외하고 대체로 type 사용

--- frontend/src/components/common/data-table/Category.tsx
+++ frontend/src/components/common/data-table/Category.tsx
@@ -5,12 +5,12 @@ interface CategoryProps {
 }
 
 const Category = ({ type }: CategoryProps) => {
-  const { label, bg, text } = CATEGORY_STYLE[type];
+  const { bg, text } = CATEGORY_STYLE[type];
   return (
     <button
       className={`inline-flex items-center rounded-sm ${bg} border-radius-md cursor-pointer px-1.5 py-0.75`}
     >
-      <p className={`category-regular ${text}`}>{label}</p>
+      <p className={`category-semibold ${text}`}>{type}</p>
     </button>
   );
 };
--- frontend/src/types/category.ts
+++ frontend/src/types/category.ts
@@ -13,59 +13,47 @@ export type CategoryType =
 export const CATEGORY_STYLE: Record<
   CategoryType,
   {
-    label: string;
     bg: string;
     text: string;
   }
 > = {
   거주: {
-    label: '거주',
     bg: 'bg-muted-mauve-50/10',
     text: 'text-muted-mauve-50',
   },
   식비: {
-    label: '식비',
     bg: 'bg-dusty-rose-50/10',
     text: 'text-dusty-rose-50',
   },
   교통비: {
-    label: '교통비',
     bg: 'bg-warm-tan-50/10',
     text: 'text-warm-tan-50',
   },
-
   생활: {
-    label: '생활',
     bg: 'bg-antique-gold-50/10',
     text: 'text-antique-gold-50',
   },
   여가: {
-    label: '여가',
     bg: 'bg-olive-green-50/10',
     text: 'text-olive-green-50',
   },
   쇼핑: {
-    label: '쇼핑',
     bg: 'bg-sea-green-50/10',
     text: 'text-sea-green-50',
   },
   통신비: {
-    label: '통신비',
     bg: 'bg-blue-grey-50/10',
     text: 'text-blue-grey-50',
   },
   학교: {
-    label: '학교',
     bg: 'bg-steel-blue-50/10',
     text: 'text-steel-blue-50',
   },
   수입: {
-    label: '수입',
     bg: 'bg-periwinkle-blue-50/10',
     text: 'text-periwinkle-blue-50',
   },
   미분류: {
-    label: '미분류',
     bg: 'bg-label-alternative/10',
     text: 'text-label-alternative',
   },

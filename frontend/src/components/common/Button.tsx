97181484e3eeb63da442fbbe0b8fd893fdc85a0f
Kjiw0n <128016888+Kjiw0n@users.noreply.github.com>
2026-01-26 17:55:57 +0900

    [FEAT] Button, Filter 공통 컴포넌트 구현 (#108)
    * style: radius style 추가
    
    * feat: Button 공통 컴포넌트 구성
    
    - variant?: 'outlined' | 'solid'
    
    - size?: 'xs' | 'sm' | 'md' | 'lg'
    
    - disabled?: boolean
    
    - label: string
    
    - default props: variant = 'outlined', size = 'sm', disabled = false
    
    * chore: add Icon to assets/ (caret-down, close)
    
    * feat: Button컴포넌트 추가
    
    - variant?: 'outlined' | 'solid';
      size?: 'xs' | 'sm' | 'md' | 'lg';
      disabled?: boolean;
      label: string;
      onClick?: React.MouseEventHandler<HTMLButtonElement>;
    
    * feat: Filter 컴포넌트 추가
    
    - size?: 'xs' | 'sm' | 'md';
      disabled?: boolean;
      active?: boolean;
      label: string;
      isOpen?: boolean;
      onClick?: React.MouseEventHandler<HTMLButtonElement>;
    
    * feat: Button 컴포넌트에 variant='danger'의 경고성 타입 추가 (빨간 배경색 버튼)
    
    * style: diabled 시 배경색 제거
    
    * refactor: Button, Filter 컴포넌트의 label props -> children으로 처리
    
    * style: update font style

---
+++ frontend/src/components/common/Button.tsx
@@ -0,0 +1,59 @@
+import clsx from 'clsx';
+
+interface ButtonProps {
+  variant?: 'outlined' | 'solid' | 'danger';
+  size?: 'xs' | 'sm' | 'md' | 'lg';
+  disabled?: boolean;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  children: React.ReactNode;
+}
+
+const Button = ({
+  variant = 'outlined',
+  size = 'sm',
+  disabled = false,
+  onClick,
+  children,
+}: ButtonProps) => {
+  const buttonClass = clsx(
+    'w-fit flex items-center justify-center box-border transition-colors',
+
+    /* --- disabled --- */
+    disabled && 'cursor-not-allowed text-label-assistive',
+
+    /* --- outlined --- */
+    !disabled &&
+      variant === 'outlined' &&
+      'border border-line-normal-neutral text-label-neutral hover:bg-fill-alternative cursor-pointer',
+
+    disabled && variant === 'outlined' && 'border border-line-normal-neutral ',
+
+    /* --- solid --- */
+    !disabled &&
+      variant === 'solid' &&
+      'bg-primary-normal text-inverse-label hover:bg-primary-strong cursor-pointer',
+
+    disabled && variant === 'solid' && 'bg-interactive-disable',
+
+    /* --- danger --- */
+    !disabled &&
+      variant === 'danger' &&
+      'bg-status-negative text-inverse-label hover:bg-status-negative-strong cursor-pointer',
+
+    disabled && variant === 'danger' && 'bg-status-negative/10',
+
+    /* --- size --- */
+    size === 'xs' && 'px-[5px] py-1 h-[26px] label2-medium rounded-modal-6',
+    size === 'sm' && 'px-[14px] py-[7px] h-8 label2-medium rounded-modal-8',
+    size === 'md' && 'px-5 py-[9px] h-10 body2-normal-medium rounded-modal-10',
+    size === 'lg' && 'px-7 py-3 h-12 body1-normal-bold rounded-modal-12',
+  );
+
+  return (
+    <button className={buttonClass} disabled={disabled} onClick={onClick}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
---
+++ frontend/src/components/common/Filter.tsx
@@ -0,0 +1,70 @@
+import clsx from 'clsx';
+
+import { Icons } from '@/assets';
+
+interface FilterProps {
+  size?: 'xs' | 'sm' | 'md';
+  disabled?: boolean;
+  active?: boolean;
+  isOpen?: boolean;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  children: React.ReactNode;
+}
+
+const Filter = ({
+  size = 'sm',
+  disabled = false,
+  active = false,
+  isOpen = false,
+  onClick,
+  children,
+}: FilterProps) => {
+  const FilterClass = clsx(
+    'w-fit max-w-37.5 flex items-center justify-center gap-0.5 box-border transition-colors',
+    'border cursor-pointer',
+
+    /* --- disabled --- */
+    disabled &&
+      'border-line-normal-normal text-label-disable cursor-not-allowed',
+
+    /* --- active --- */
+    !disabled &&
+      active &&
+      'border-primary-normal text-primary-normal hover:bg-primary-normal/5',
+
+    !disabled && active && isOpen && 'bg-primary-normal/10',
+
+    /* --- inactive --- */
+    !disabled &&
+      !active &&
+      'border-line-normal-normal text-label-neutral hover:bg-fill-alternative',
+
+    !disabled && !active && isOpen && 'bg-fill-strong',
+
+    /* --- size --- */
+    size === 'xs' &&
+      'pl-[7px] pr-[5px] py-1 h-6 caption1-medium rounded-modal-6',
+    size === 'sm' &&
+      'pl-2 pr-[6px] py-[6px] h-8 label1-normal-medium rounded-modal-8',
+    size === 'md' &&
+      'pl-[11px] pr-[9px] py-[7px] h-9 body2-normal-medium rounded-modal-10',
+  );
+
+  return (
+    <button className={FilterClass} disabled={disabled} onClick={onClick}>
+      <span className="max-w-25 overflow-hidden px-0.5">{children}</span>
+      {active ? (
+        <Icons.Close className="h-3.5 w-3.5" />
+      ) : (
+        <Icons.CaretDown
+          className={clsx(
+            'h-2 w-2 transition-transform duration-300',
+            isOpen && 'rotate-180',
+          )}
+        />
+      )}
+    </button>
+  );
+};
+
+export default Filter;

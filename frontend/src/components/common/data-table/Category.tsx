daa218ce980eb7f68c00535a888b03fd46aba52c
1jiwoo27 <chloey1227@gmail.com>
2026-01-26 17:09:34 +0900

    chore: prettier 스크립트 실행

--- frontend/src/components/common/data-table/Category.tsx
+++ frontend/src/components/common/data-table/Category.tsx
@@ -7,9 +7,7 @@ interface CategoryProps {
 const Category = ({ type }: CategoryProps) => {
   const { bg, text } = CATEGORY_STYLE[type];
   return (
-    <div
-      className={`inline-flex items-center rounded-md ${bg} px-1.5 py-0.75`}
-    >
+    <div className={`inline-flex items-center rounded-md ${bg} px-1.5 py-0.75`}>
       <span className={`category-semibold ${text}`}>{type}</span>
     </div>
   );

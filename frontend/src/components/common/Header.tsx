1ffe6d0661263920ae5a767f52dd85f961c8268b
1jiwoo27 <chloey1227@gmail.com>
2026-01-27 16:54:12 +0900

    feat: 현재 시간 불러오는 util 함수 추가 및 헤더에 적용

--- frontend/src/components/common/Header.tsx
+++ frontend/src/components/common/Header.tsx
@@ -1,14 +1,16 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import Dropdown from '@/components/common/dropdown/Dropdown';
 
 import { Icons } from '@/assets';
 import ProfileImage from '@/assets/images/profile.png';
+import { getLocalTime } from '@/lib/utils';
 
 import Button from './Button';
 
 const Header = () => {
   const [selected, setSelected] = useState<string | null>(null);
+  const [time, setTime] = useState(getLocalTime());
 
   // @TODO: 추후 options API 연동
   const options = [
@@ -17,17 +19,25 @@ const Header = () => {
     { id: '3', name: '독일 교환학생' },
   ];
 
+  // 1분마다 한국 시간 갱신
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(getLocalTime());
+    }, 60_000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <div className="flex justify-between px-8 py-3 sticky top-0 bg-background-alternative z-10 border-b border-line-solid-normal">
+    <div className="sticky top-0 z-10 flex justify-between border-b border-line-solid-normal bg-background-alternative px-8 py-3">
       <Dropdown
         selected={selected}
         onSelect={setSelected}
         options={options}
       />
-      <div className="flex gap-5 items-center">
+      <div className="flex items-center gap-5">
         <div className="flex items-center gap-2">
-          <span className="label2-medium text-label-neutral">오전 09:05</span>
-          <Icons.Refresh className="w-4 h-4 text-label-neutral" />
+          <span className="label2-medium text-label-neutral">{time}</span>
+          <Icons.Refresh className="h-4 w-4 cursor-pointer text-label-neutral"/>
         </div>
         <Button>모바일</Button>
         <img

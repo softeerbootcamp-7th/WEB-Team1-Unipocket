eff181911727e4c2cf07edab9802927686f3430c
1jiwoo27 <chloey1227@gmail.com>
2026-01-29 15:44:38 +0900

    feat: clear 버튼 추가 (focus & active 상태에서는 clear 버튼이 보이도록)

--- frontend/src/components/common/TextInput.tsx
+++ frontend/src/components/common/TextInput.tsx
@@ -24,18 +24,16 @@ const TextInput = ({
   return (
     <div className="flex w-85 flex-col gap-2">
       {title && (
-        <p className="label1-normal-bold text-label-neutral">
-          {title}
-        </p>
+        <p className="label1-normal-bold text-label-neutral">{title}</p>
       )}
 
       <div className="relative">
         <input
           className={clsx(
-            'w-full p-3 pr-10 body2-normal-regular border border-solid rounded-xl outline-none text-label-normal',
+            'body2-normal-regular text-label-normal w-full rounded-xl border border-solid p-3 pr-10 outline-none',
             isError
               ? 'border-status-negative'
-              : 'border-line-normal-neutral focus:border-primary-normal'
+              : 'border-line-normal-neutral focus:border-primary-normal',
           )}
           value={value}
           placeholder={placeholder}
@@ -45,15 +43,24 @@ const TextInput = ({
           onChange={(e) => setValue(e.target.value)}
         />
 
+        {showClearButton && (
+          <button
+            type="button"
+            onMouseDown={(e) => e.preventDefault()}
+            onClick={() => setValue('')}
+            className="absolute top-1/2 right-3 -translate-y-1/2"
+          >
+            <Icons.CloseCircle className="text-label-neutral h-5.5 w-5.5" />
+          </button>
+        )}
+
         {isError && !showClearButton && (
-            <Icons.AlertCircle className="absolute right-3 top-1/2 -translate-y-1/2 h-5.5 w-5.5" />
-      )}
+          <Icons.AlertCircle className="absolute top-1/2 right-3 h-5.5 w-5.5 -translate-y-1/2" />
+        )}
       </div>
 
       {isError && errorMessage && (
-        <p className="caption1-regular text-status-negative">
-          {errorMessage}
-        </p>
+        <p className="caption1-regular text-status-negative">{errorMessage}</p>
       )}
     </div>
   );

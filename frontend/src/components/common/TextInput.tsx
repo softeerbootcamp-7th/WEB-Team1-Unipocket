bb85b2ca490f9cb3531a2d9a831fcd3b9bf84fb7
1jiwoo27 <chloey1227@gmail.com>
2026-01-29 15:43:39 +0900

    feat: input 컴포넌트 구현 및 에러 상태 추가

--- frontend/src/components/common/TextInput.tsx
+++ frontend/src/components/common/TextInput.tsx
@@ -1,17 +1,62 @@
+import { useState } from 'react';
+import clsx from 'clsx';
+
+import { Icons } from '@/assets';
+
 interface TextInputProps {
-  placeholder?: string;
+  title?: string;
+  placeholder: string;
+  errorMessage?: string;
+  isError?: boolean;
 }
 
-const TextInput = ({ placeholder="텍스트를 입력해 주세요." }: TextInputProps) => {
+const TextInput = ({
+  title,
+  placeholder,
+  errorMessage,
+  isError = false,
+}: TextInputProps) => {
+  const [value, setValue] = useState('');
+  const [isFocused, setIsFocused] = useState(false);
+
+  const showClearButton = value.length > 0 && isFocused;
+
   return (
-    <div className="flex flex-col gap-2">
-        <p>주제</p>
-        <input
-            placeholder={placeholder}
-        ></input>
-        <p>메시지에 마침표를 찍어요.</p>
-    </div>
-  )
-}
+    <div className="flex w-85 flex-col gap-2">
+      {title && (
+        <p className="label1-normal-bold text-label-neutral">
+          {title}
+        </p>
+      )}
 
-export default TextInput
+      <div className="relative">
+        <input
+          className={clsx(
+            'w-full p-3 pr-10 body2-normal-regular border border-solid rounded-xl outline-none text-label-normal',
+            isError
+              ? 'border-status-negative'
+              : 'border-line-normal-neutral focus:border-primary-normal'
+          )}
+          value={value}
+          placeholder={placeholder}
+          autoComplete="off"
+          onFocus={() => setIsFocused(true)}
+          onBlur={() => setIsFocused(false)}
+          onChange={(e) => setValue(e.target.value)}
+        />
+
+        {isError && !showClearButton && (
+            <Icons.AlertCircle className="absolute right-3 top-1/2 -translate-y-1/2 h-5.5 w-5.5" />
+      )}
+      </div>
+
+      {isError && errorMessage && (
+        <p className="caption1-regular text-status-negative">
+          {errorMessage}
+        </p>
+      )}
+    </div>
+  );
+};
+
+export default TextInput;

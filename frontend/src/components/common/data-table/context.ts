4cb064df73588864bbfe581b92a82d4591eb2850
Mingyu Seong <minngyuseong@gmail.com>
2026-01-30 16:33:22 +0900

    fix: any 타입 제거 및 제네릭 타입 적용

--- frontend/src/components/common/data-table/context.ts
+++ frontend/src/components/common/data-table/context.ts
@@ -1,23 +1,26 @@
+// context.tsx
 import { createContext, useContext } from 'react';
 import type { Table } from '@tanstack/react-table';
 
 import type { TableUIAction, TableUIState } from './type';
 
-interface DataTableContextType<TData> {
+export interface DataTableContextType<TData, TValue> {
   table: Table<TData>;
-  tableState: TableUIState;
-  dispatch: React.Dispatch<TableUIAction>;
+  tableState: TableUIState<TValue>;
+  dispatch: React.Dispatch<TableUIAction<TValue>>;
 }
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const DataTableContext = createContext<DataTableContextType<any> | null>(
-  null,
-);
+export const DataTableContext = createContext<DataTableContextType<
+  unknown,
+  unknown
+> | null>(null);
 
-export const useDataTable = () => {
+export const useDataTable = <TData, TValue>() => {
   const context = useContext(DataTableContext);
+
   if (!context) {
     throw new Error('useDataTable must be used within a DataTableProvider');
   }
-  return context;
+
+  return context as DataTableContextType<TData, TValue>;
 };
--- frontend/src/routes/_app.travel.tsx
+++ frontend/src/routes/_app.travel.tsx
@@ -1,11 +1,12 @@
 import { createFileRoute } from '@tanstack/react-router';
 
+import CellEditor from '@/components/common/data-table/CellEditor';
 import { DataTable } from '@/components/common/data-table/DataTable';
 import DataTableProvider from '@/components/common/data-table/DataTableProvider';
 import SelectionActionProvider from '@/components/common/data-table/SelectionActionProvider';
 
 import { columns } from '@/payments/columns';
-import { getData } from '@/payments/dummy';
+import { type Expense, getData } from '@/payments/dummy';
 
 export const Route = createFileRoute('/_app/travel')({
   component: RouteComponent,
@@ -15,13 +16,18 @@ function RouteComponent() {
   const data = getData();
   return (
     <div className="p-10">
-      <DataTableProvider
-        columns={columns}
-        data={data}
-        floatingBarVariant={'MANAGEMENT'}
-      >
-        <DataTable />
+      <DataTableProvider<Expense> columns={columns} data={data}>
+        <DataTable<Expense>
+          groupBy={(row) =>
+            new Date(row.date).toLocaleDateString('ko-KR', {
+              year: 'numeric',
+              month: '2-digit',
+              day: '2-digit',
+            })
+          }
+        />
         <SelectionActionProvider />
+        <CellEditor />
       </DataTableProvider>
     </div>
   );

c24f768db6c26a4bf5c0e03a679577eb9a50b204
Chanwoo Kim <kcw2205@naver.com>
2026-02-01 23:10:30 +0900

    [BE] [Global] 국가코드와 화폐코드 Enum 클래스 추가 (#170)

---
+++ backend/src/main/java/com/genesis/unipocket/global/common/enums/CurrencyCode.java
@@ -0,0 +1,59 @@
+package com.genesis.unipocket.global.common.enums;
+
+/**
+ * <b>화폐 코드 Enum</b>
+ * <p>
+ * 네이버 환율 표시 기준에 따라 작성한 화폐 코드 리스트 입니다. <br>
+ * </p>
+ * @author bluefishez
+ * @since 2026-01-30
+ */
+public enum CurrencyCode {
+	KRW,
+	ZAR,
+	NPR,
+	NOK,
+	NZD,
+	TWD,
+	DKK,
+	RUB,
+	MOP,
+	MYR,
+	MXN,
+	MNT,
+	USD,
+	BHD,
+	BDT,
+	VND,
+	BRL,
+	BND,
+	SAR,
+	SEK,
+	CHF,
+	SGD,
+	AED,
+	GBP,
+	OMR,
+	JOD,
+	EUR,
+	ILS,
+	EGP,
+	INR,
+	IDR,
+	JPY,
+	CNY,
+	CZK,
+	CLP,
+	KZT,
+	QAR,
+	CAD,
+	KWD,
+	THB,
+	TRY,
+	PKR,
+	PLN,
+	PHP,
+	HUF,
+	AUD,
+	HKD;
+}

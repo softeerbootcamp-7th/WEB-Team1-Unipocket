7bfc4eabceb2d59781ae6e574b201a4e88809843
1jiwoo27 <chloey1227@gmail.com>
2026-02-02 13:57:08 +0900

    feat: title textarea 높이 계산을 위한 ref 추가

--- frontend/src/components/common/side-panel/SidePanel.tsx
+++ frontend/src/components/common/side-panel/SidePanel.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useLayoutEffect, useRef, useState } from 'react';
 
 import Button from '@/components/common/Button';
 import Divider from '@/components/common/Divider';
@@ -9,22 +9,43 @@ import CurrencyConverter from './CurrencyConverter';
 import ValueContainer from './ValueContainer';
 
 const SidePanel = () => {
+  const [title, setTitle] = useState('');
   const [memo, setMemo] = useState('');
 
-  const title = 'Coles (Wollongong Central) Groceries';
+  const ref = useRef<HTMLTextAreaElement>(null);
+
+  useLayoutEffect(() => {
+    if (!ref.current) return;
+    ref.current.style.height = '0px';
+    ref.current.style.height = `${ref.current.scrollHeight}px`;
+  }, [title]);
+
   const showUploader = true;
 
   return (
-    <div className="flex w-100 h-screen overflow-y-auto scrollbar fixed right-0 top-0 z-50 flex-col gap-8 pb-50 border-l border-line-normal-normal bg-background-normal shadow-panel">
-      <div className="flex p-4 justify-between items-center">
-        <Icon color="text-label-neutral" iconName="ChevronForward" width={24} height={24} />
+    <div className="scrollbar border-line-normal-normal bg-background-normal shadow-panel fixed top-0 right-0 z-50 flex h-screen w-100 flex-col gap-8 overflow-y-auto border-l pb-50">
+      <div className="flex items-center justify-between p-4">
+        <Icon
+          color="text-label-neutral"
+          iconName="ChevronForward"
+          width={24}
+          height={24}
+        />
         <div className="flex gap-2">
-            <Button>저장</Button>
-            <Button>삭제</Button>
+          <Button>저장</Button>
+          <Button>삭제</Button>
         </div>
       </div>
       <div className="flex flex-col gap-10 px-5">
-        <div className="heading1-bold text-label-strong word-break">{title}</div>
+        <textarea
+          ref={ref}
+          className="heading1-bold text-label-strong resize-none overflow-hidden border-0 bg-transparent leading-tight outline-0"
+          value={title}
+          placeholder="제목을 입력해 주세요."
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+        />
         <ValueContainer />
         <Divider style="thin" />
         <CurrencyConverter />
@@ -35,9 +56,12 @@ const SidePanel = () => {
           title="메모"
           placeholder="메모를 입력해 주세요."
         />
-        
+
         {showUploader && (
-          <><Divider style="thin" /><div>업로더</div></>
+          <>
+            <Divider style="thin" />
+            <div>업로더</div>
+          </>
         )}
       </div>
     </div>

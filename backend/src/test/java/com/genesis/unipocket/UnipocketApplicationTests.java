1aa3d6c76e7f9a14c032538113ac7cd962575d6e
김현창 <hyunchang52@gmail.com>
2026-02-02 18:06:46 +0900

    [BE] [Global] 프로파일 분리 (#182)
    * refactor(global): 개발환경 프로파일 분리
    - 공통 프로파일(common)과 개발서버 프로파일(dev) 분리
    - .gitignore에 로컬 프로파일 추가(application-local.yml)
    
    # Conflicts:
    #	backend/src/main/resources/application.yml
    
    * feat(global): 테스트용 프로파일 생성
    - 단위테스트용 프로파일 생성
    - 통합테스트용 프로파일 생성
    - 컨텍스트 로드 테스트 활성화

--- backend/src/test/java/com/genesis/unipocket/UnipocketApplicationTests.java
+++ backend/src/test/java/com/genesis/unipocket/UnipocketApplicationTests.java
@@ -1,13 +1,13 @@
 package com.genesis.unipocket;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
-@Disabled
+@ActiveProfiles("test-it")
 class UnipocketApplicationTests {
 
 	@Test

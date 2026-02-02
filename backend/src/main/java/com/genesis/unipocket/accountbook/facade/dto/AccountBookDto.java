b4e5a768090ca6b5e61c0cc88ee9b4a0117bef7f
blu3fishez <kcw2205@naver.com>
2026-02-02 20:20:34 +0900

    refactor(account_book): 퍼사드 DTO record 로 변경

--- backend/src/main/java/com/genesis/unipocket/accountbook/facade/dto/AccountBookDto.java
+++ backend/src/main/java/com/genesis/unipocket/accountbook/facade/dto/AccountBookDto.java
@@ -4,8 +4,6 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.genesis.unipocket.accountbook.persistence.entity.AccountBookEntity;
 import com.genesis.unipocket.global.common.enums.CountryCode;
 import java.time.LocalDate;
-import lombok.Builder;
-import lombok.Getter;
 
 /**
  * <b>가계부 공통 DTO</b>
@@ -13,30 +11,19 @@ import lombok.Getter;
  * 퍼사드 <-> 서비스 간 통신에 사용되는 DTO
  * 가계부 퍼사드 계층에서 입출력에 사용되는 DTO
  * </p>
+ *
  * @author bluefishez
  * @since 2026-01-30
  */
-@Builder
-@Getter
-public class AccountBookDto {
-
-	private final Long id;
-
-	private final Long userId;
-
-	private final String title;
-
-	private final CountryCode localCountryCode;
-
-	private final CountryCode baseCountryCode;
-
-	private final Integer budget;
-
-	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
-	private final LocalDate startDate;
-
-	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
-	private final LocalDate endDate;
+public record AccountBookDto(
+		Long id,
+		Long userId,
+		String title,
+		CountryCode localCountryCode,
+		CountryCode baseCountryCode,
+		Integer budget,
+		@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd") LocalDate startDate,
+		@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd") LocalDate endDate) {
 
 	public static AccountBookDto from(AccountBookEntity entity) {
 		var builder =

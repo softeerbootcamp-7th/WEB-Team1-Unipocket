57699e3b35d7bf36e9c529f9e567e8e34da0927f
1jiwoo27 <chloey1227@gmail.com>
2026-02-03 18:20:21 +0900

    chore: gap 대신 leading-relaxed 속성 사용

--- frontend/src/components/common/upload/UploadBox.tsx
+++ frontend/src/components/common/upload/UploadBox.tsx
@@ -7,10 +7,8 @@ const UploadBox = () => {
       <h3 className="body2-normal-bold text-label-alternative text-center whitespace-pre-line">
         {'여기에 파일을 드래그하거나\n클릭하여 업로드하세요.'}
       </h3>
-      <p className="caption1-medium text-label-alternative flex flex-col gap-1 text-center">
-        <span>지원 형식: jpg, jpeg, png, heic</span>
-        <span>최대 30개까지 업로드할 수 있어요.</span>
-        <span>업로드 시 전체 파일 용량 합계는 50MB를 넘을 수 없어요.</span>
+      <p className="caption1-medium text-label-alternative text-center whitespace-pre-line leading-relaxed">
+        {'지원 형식: jpg, jpeg, png, heic\n최대 30개까지 업로드할 수 있어요.\n업로드 시 전체 파일 용량 합계는 50MB를 넘을 수 없어요.'}
       </p>
     </div>
   );

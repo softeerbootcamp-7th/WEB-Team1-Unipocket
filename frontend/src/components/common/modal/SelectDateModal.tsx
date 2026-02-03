76148d2e0188f19ad1451cb6a60a59d674a8fa8f
Kjiw0n <rwd0904@naver.com>
2026-02-03 14:30:59 +0900

    feat: SelectDateModal 유효성 로직 추가(startDate, endDate 모두 선택해야 버튼 활성화)

--- frontend/src/components/common/modal/SelectDateModal.tsx
+++ frontend/src/components/common/modal/SelectDateModal.tsx
@@ -1,11 +1,13 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
-import Calendar from '@/components/common/calendar/Calendar';
+import Calendar, {
+  type DateRange,
+} from '@/components/common/calendar/Calendar';
 
 import { formatDateWithDay } from '@/lib/utils';
 
-import type { DateRange } from '../calendar/CalendarMonthView';
 import Modal, { type ModalProps } from './Modal';
+import { useModalContext } from './useModalContext';
 
 interface DatePickItemProps {
   label: string;
@@ -30,6 +32,48 @@ const DatePickItem = ({ label, date }: DatePickItemProps) => {
           날짜를 선택해주세요
         </span>
       )}
+    </div>
+  );
+};
+
+interface SelectDateContentProps extends DateRange {
+  onChange: (startDate: Date | null, endDate: Date | null) => void;
+}
+
+const SelectDateContent = ({
+  startDate,
+  endDate,
+  onChange,
+}: SelectDateContentProps) => {
+  const { setActionReady } = useModalContext();
+
+  // 여기서 useEffect로 버튼 제어
+  useEffect(() => {
+    setActionReady(!!startDate && !!endDate);
+  }, [startDate, endDate, setActionReady]);
+
+  return (
+    <div className="flex flex-col gap-8">
+      {/* text section */}
+      <div className="flex flex-col items-center gap-2.5">
+        <h2 className="text-label-normal headline1-bold">
+          카드 연동 기간 설정
+        </h2>
+        <span className="text-label-alternative body1-normal-medium text-center">
+          언제부터 카드 내역을 불러올까요? <br />
+          교환학생 지출을 기록할 기간을 선택해주세요
+        </span>
+      </div>
+
+      {/* calendar section */}
+      <div className="flex flex-col gap-3 px-4">
+        <div className="flex items-center justify-center gap-2.5">
+          <DatePickItem label="시작일" date={startDate} />
+          <DatePickItem label="종료일" date={endDate} />
+        </div>
+
+        <Calendar startDate={startDate} endDate={endDate} onChange={onChange} />
+      </div>
     </div>
   );
 };
@@ -59,32 +103,11 @@ const SelectDateModal = ({
 
   return (
     <Modal {...modalProps}>
-      <div className="flex flex-col gap-8">
-        {/* text section */}
-        <div className="flex flex-col items-center gap-2.5">
-          <h2 className="text-label-normal headline1-bold">
-            카드 연동 기간 설정
-          </h2>
-          <span className="text-label-alternative body1-normal-medium text-center">
-            언제부터 카드 내역을 불러올까요? <br />
-            교환학생 지출을 기록할 기간을 선택해주세요
-          </span>
-        </div>
-
-        {/* calendar section */}
-        <div className="flex flex-col gap-3 px-4">
-          <div className="flex items-center justify-center gap-2.5">
-            <DatePickItem label="시작일" date={startDate} />
-            <DatePickItem label="종료일" date={endDate} />
-          </div>
-
-          <Calendar
-            startDate={startDate}
-            endDate={endDate}
-            onChange={handleDatesChange}
-          />
-        </div>
-      </div>
+      <SelectDateContent
+        startDate={startDate}
+        endDate={endDate}
+        onChange={handleDatesChange}
+      />
     </Modal>
   );
 };

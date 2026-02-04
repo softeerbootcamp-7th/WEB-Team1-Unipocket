067225249180091d057ccefc57469ef5c262eb9b
codingbaraGo <hyunchang52@gmail.com>
2026-02-04 11:12:14 +0900

    feat(expense): Application 계층 개발 - Service 클래스 개발 - DTO & Converter 개발

---
+++ backend/src/main/java/com/genesis/unipocket/expense/application/dto/ExpenseManualCreateCommand.java
@@ -0,0 +1,24 @@
+package com.genesis.unipocket.expense.application.dto;
+
+import com.genesis.unipocket.expense.common.enums.Category;
+import com.genesis.unipocket.global.common.enums.CurrencyCode;
+import java.time.LocalDateTime;
+
+/**
+ * <b>지출내역 생성 - 수기 DTO</b>
+ * <p>Facade -> Application
+ *
+ * @author codingbaraGo
+ * @since 2026-02-03
+ */
+public record ExpenseManualCreateCommand(
+		Long accountBookId,
+		String merchantName,
+		Category category,
+		String paymentMethod,
+		LocalDateTime occurredAt,
+		Double localAmount,
+		CurrencyCode localCurrency,
+		Double standardAmount,
+		CurrencyCode standardCurrency,
+		String memo) {}

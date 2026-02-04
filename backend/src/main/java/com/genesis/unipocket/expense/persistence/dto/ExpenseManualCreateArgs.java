d37f01357e1ee73176509de987cbb3b82a021d4f
codingbaraGo <hyunchang52@gmail.com>
2026-02-04 10:47:06 +0900

    feat(expense): Persistence 계층 개발 - Expense 생성용 DTO 정의 - 레포지토리 정의

---
+++ backend/src/main/java/com/genesis/unipocket/expense/persistence/dto/ExpenseManualCreateArgs.java
@@ -0,0 +1,24 @@
+package com.genesis.unipocket.expense.persistence.dto;
+
+import com.genesis.unipocket.expense.common.enums.Category;
+import com.genesis.unipocket.global.common.enums.CurrencyCode;
+import java.time.LocalDateTime;
+
+/**
+ * <b>지출내역 생성 - 수기 DTO</b>
+ * <p>Application -> Persistence
+ *
+ * @author codingbaraGo
+ * @since 2026-02-03
+ */
+public record ExpenseManualCreateArgs(
+		Long accountBookId,
+		String merchantName,
+		Category category,
+		String paymentMethod,
+		LocalDateTime occurredAt,
+		Double localAmount,
+		CurrencyCode localCurrency,
+		Double standardAmount,
+		CurrencyCode standardCurrency,
+		String memo) {}

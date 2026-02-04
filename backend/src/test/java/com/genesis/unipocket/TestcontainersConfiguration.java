83f7f2bedff212eaa6134c79a8fb39f38dcf246d
김현창 <hyunchang52@gmail.com>
2026-02-04 21:54:31 +0900

    [BE] [CI] 백엔드 CI 테스트 에러 수정 (#238)
    * fix(test): 통합 테스트 설정 변경
    - TestContainer 설정에 redis 설정 추가
    - 통합 테스트 프로파일 설정
    
    * fix(test): 프로파일 파일 수정
    - Oauth2 관련 property 추가
    - Redis 설정 추가
    
    * fix(test): 누락된 redis 의존성 추가
    
    * refactor(test): 통합 테스트와 단위 테스트 분리
    
    * refactor(test): 통합 테스트 workflow와 단위 테스트 workflow 분리
    
    * refactor(test): 통합 테스트 workflow job 이름, paths 수정
    
    * fix: 테스트 별 프로파일 분리
    
    * chore: resolve conflict

--- backend/src/test/java/com/genesis/unipocket/TestcontainersConfiguration.java
+++ backend/src/test/java/com/genesis/unipocket/TestcontainersConfiguration.java
@@ -3,15 +3,23 @@ package com.genesis.unipocket;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
-class TestcontainersConfiguration {
+public class TestcontainersConfiguration {
 
 	@Bean
 	@ServiceConnection
 	MySQLContainer<?> mysqlContainer() {
 		return new MySQLContainer<>(DockerImageName.parse("mysql:8.4.8"));
+	}
+
+	@Bean
+	@ServiceConnection(name = "redis")
+	GenericContainer<?> redisContainer() {
+		return new GenericContainer<>(DockerImageName.parse("redis:7-alpine"))
+				.withExposedPorts(6379);
 	}
 }

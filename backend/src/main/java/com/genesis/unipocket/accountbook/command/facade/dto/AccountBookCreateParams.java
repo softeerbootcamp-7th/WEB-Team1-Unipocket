87ddd32fd44b4e35cc479bf2e90796946a4b5181
blu3fishez <kcw2205@naver.com>
2026-02-05 00:48:24 +0900

    refactor(account_book): 사용하지 않는 DTO 삭제

--- backend/src/main/java/com/genesis/unipocket/accountbook/command/facade/dto/AccountBookCreateParams.java
+++
@@ -1,7 +0,0 @@
-package com.genesis.unipocket.accountbook.command.facade.dto;
-
-import com.genesis.unipocket.global.common.enums.CountryCode;
-import java.time.LocalDate;
-
-public record AccountBookCreateParams(
-		CountryCode localCountryCode, LocalDate startDate, LocalDate endDate) {}

41a47446bd25402999e3d1d26fba88c4dd357936
Mingyu Seong <minngyuseong@gmail.com>
2026-02-05 16:50:46 +0900

    chore: getCountryInfo 사용시 에러 핸들링 로직 추가

--- frontend/src/components/common/CurrencyBadge.tsx
+++ frontend/src/components/common/CurrencyBadge.tsx
@@ -8,15 +8,17 @@ interface CurrencyBadgeProps {
 const CurrencyBadge = ({ countryCode }: CurrencyBadgeProps) => {
   const countryInfo = getCountryInfo(countryCode);
 
+  if (!countryInfo) return null;
+
   return (
     <div className="flex items-center gap-1">
       <img
         className="h-3 w-4"
-        src={countryInfo?.imageUrl}
-        alt={`${countryInfo?.currencyName} flag`}
+        src={countryInfo.imageUrl}
+        alt={`${countryInfo.currencyName} flag`}
       />
       <span className="text-label-neutral text-[13px]">
-        {countryInfo?.currencyName}
+        {countryInfo.currencyName}
       </span>
     </div>
   );

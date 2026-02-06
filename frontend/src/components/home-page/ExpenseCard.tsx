18a2fdaa0f9ba13a3617fb85e7c850c0a978bcc8
Kjiw0n <rwd0904@naver.com>
2026-02-06 15:35:26 +0900

    style: add items-end (ExpenseCard)

--- frontend/src/components/home-page/ExpenseCard.tsx
+++ frontend/src/components/home-page/ExpenseCard.tsx
@@ -25,7 +25,7 @@ const ExpenseCard = ({
         <span className="body2-normal-medium">{label}</span>
         <Icons.Information className="size-4" />
       </div>
-      <div className="flex gap-3">
+      <div className="flex items-end gap-3">
         <CurrencyAmountDisplay
           countryCode={baseCountryCode}
           amount={baseCountryAmount}

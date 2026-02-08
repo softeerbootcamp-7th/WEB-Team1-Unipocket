895a84f68a115ec7036995f27206d479e8fccea3
Kjiw0n <rwd0904@naver.com>
2026-02-08 18:22:43 +0900

    chore: expense chart에 사용되는 type 정의(ExpenseChartMode, ExpenseChartUiItem)

--- frontend/src/components/common/chart/chartType.ts
+++ frontend/src/components/common/chart/chartType.ts
@@ -1,5 +1,30 @@
 import type { CurrencyType } from '@/types/currency';
 
+// type & interface
+export type ExpenseChartMode = 'method' | 'currency';
+
+export interface ExpenseChartUiItem {
+  id: string | number; // 리스트 렌더링 key용
+  label: string;
+  percent: number;
+  color: string;
+  subLabel?: string;
+}
+
+// option
+export const CURRENCY_OPTIONS: {
+  id: number;
+  name: string;
+  type: CurrencyType;
+}[] = [
+  { id: 1, name: '기준 통화', type: 'BASE' },
+  { id: 2, name: '현지 통화', type: 'LOCAL' },
+];
+
+/** constants */
+export const TOTAL_ANIMATION_DURATION = 0.8;
+
+/** colors */
 export const CATEGORY_CHART_COLORS = [
   'var(--color-teal-40)',
   'var(--color-teal-50)',
@@ -22,16 +47,3 @@ export const EXPENSE_SUSPENSE_COLOR = [
   'var(--color-fill-strong)',
   'var(--color-fill-normal)',
 ];
-
-export const CURRENCY_OPTIONS: {
-  id: number;
-  name: string;
-  type: CurrencyType;
-}[] = [
-  { id: 1, name: '기준 통화', type: 'BASE' },
-  { id: 2, name: '현지 통화', type: 'LOCAL' },
-];
-
-export type ExpenseChartMode = 'method' | 'currency';
-
-export const TOTAL_ANIMATION_DURATION = 0.8;

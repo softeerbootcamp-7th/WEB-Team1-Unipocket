8d26208e2526751b673ff47066d0c9e87a6f719f
1jiwoo27 <chloey1227@gmail.com>
2026-02-09 14:14:43 +0900

    chore: mockData 구조 변경

--- frontend/src/components/common/chart/comparison/mock.ts
+++ frontend/src/components/common/chart/comparison/mock.ts
@@ -1,48 +1,27 @@
-import type { CurrencyType } from '@/types/currency';
-
-import type { CountryCode } from '@/data/countryCode';
-import { countryCode } from '@/data/countryCode';
-
-type ComparisonItem = {
-  currency: CurrencyType;
-  value: number;
-};
+import { type CountryCode,countryCode } from '@/data/countryCode';
 
 type ComparisonData = {
-  average: ComparisonItem;
-  me: ComparisonItem;
+  countryCode: CountryCode;
+  average: number;
+  me: number;
 };
 
 export type ComparisonStatisticsResponse = {
   month: number;
-  baseCountryCode: CountryCode;
-  localCountryCode: CountryCode;
   base: ComparisonData;
   local: ComparisonData;
 };
 
 export const mockData: ComparisonStatisticsResponse = {
   month: 12,
-  baseCountryCode: countryCode[0] as CountryCode, // 'KR'
-  localCountryCode: countryCode[12] as CountryCode, // 'US'
   base: {
-    average: {
-      currency: 'BASE',
-      value: 2311465,
-    },
-    me: {
-      currency: 'BASE',
-      value: 2002876,
-    },
+    countryCode: countryCode[0] as CountryCode, // 'KR'
+    average: 2311465,
+    me: 2002876,
   },
   local: {
-    average: {
-      currency: 'LOCAL',
-      value: 2234,
-    },
-    me: {
-      currency: 'LOCAL',
-      value: 2000,
-    },
+    countryCode: countryCode[12] as CountryCode, // 'US'
+    average: 2234,
+    me: 2000,
   },
 };

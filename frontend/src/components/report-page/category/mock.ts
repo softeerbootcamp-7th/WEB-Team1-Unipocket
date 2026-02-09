a1537dc64b25bbf839c17b26d9f657d486eb23f8
1jiwoo27 <chloey1227@gmail.com>
2026-02-09 18:49:08 +0900

    chore: mockdata 생성

---
+++ frontend/src/components/report-page/category/mock.ts
@@ -0,0 +1,10 @@
+export const mockData = [
+  { category: '거주', me: 302621, other: 379_210 },
+  { category: '교통비', me: 302621, other: 379210 },
+  { category: '생활', me: 302621, other: 379210 },
+  { category: '쇼핑', me: 302621, other: 379210 },
+  { category: '식비', me: 302621, other: 379210 },
+  { category: '여가', me: 302621, other: 379210 },
+  { category: '통신비', me: 302621, other: 379210 },
+  { category: '학교', me: 302621, other: 379210 },
+];

7723212e38ca323156c116cd5beb9be2be83ec20
1jiwoo27 <chloey1227@gmail.com>
2026-02-10 14:28:27 +0900

    chore: prettier 스크립트 실행

--- frontend/src/pages/AnalyticsPage.tsx
+++ frontend/src/pages/AnalyticsPage.tsx
@@ -2,8 +2,10 @@ import ReportCategory from '@/components/analytics-page/category/ReportCategory'
 
 const AnalyticsPage = () => {
   return (
-    <div className="flex p-3"><ReportCategory /></div>
-  )
-}
+    <div className="flex p-3">
+      <ReportCategory />
+    </div>
+  );
+};
 
-export default AnalyticsPage
+export default AnalyticsPage;

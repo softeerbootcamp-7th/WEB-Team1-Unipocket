eab20807afc22777ef2bb1bb70411ce5c3ef11e7
김현창 <hyunchang52@gmail.com>
2026-02-11 11:20:20 +0900

    [BE] [Exchange] 환율 관련 엔티티 정의 (#320)
    * feat(exchange): 환율 엔티티 정의
    
    * feat(exchange): 환율 레포지토리 정의
    
    (cherry picked from commit f2ad2d16a598919af92f55e7ed00522b793e09fb)

---
+++ backend/src/main/java/com/genesis/unipocket/exchange/command/persistence/repository/ExchangeRateRepository.java
@@ -0,0 +1,8 @@
+package com.genesis.unipocket.exchange.command.persistence.repository;
+
+import com.genesis.unipocket.exchange.command.persistence.entity.ExchangeRate;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ExchangeRateRepository extends JpaRepository<ExchangeRate, Long> {}

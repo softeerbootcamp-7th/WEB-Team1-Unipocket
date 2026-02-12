620e917194aa5afe60df6235bcaebf60a20051b9
blu3fishez <kcw2205@naver.com>
2026-02-12 21:23:21 +0900

    test(expense): 지출내역 통합테스트 재작성

--- backend/src/main/java/com/genesis/unipocket/global/config/WebMvcConfig.java
+++ backend/src/main/java/com/genesis/unipocket/global/config/WebMvcConfig.java
@@ -1,9 +1,11 @@
 package com.genesis.unipocket.global.config;
 
 import com.genesis.unipocket.auth.common.resolver.LoginUserArgumentResolver;
+import com.genesis.unipocket.global.common.enums.Category;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -16,5 +18,13 @@ public class WebMvcConfig implements WebMvcConfigurer {
 	@Override
 	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
 		resolvers.add(loginUserArgumentResolver);
+	}
+
+	@Override
+	public void addFormatters(FormatterRegistry registry) {
+		registry.addConverter(
+				String.class,
+				Category.class,
+				source -> Category.values()[Integer.parseInt(source)]);
 	}
 }

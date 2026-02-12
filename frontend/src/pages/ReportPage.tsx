89f7e2309f6bbee8b0942139a3b02b7996168f71
1jiwoo27 <chloey1227@gmail.com>
2026-02-12 10:10:43 +0900

    feat: accountbook 데이터 없을 시, mockdata 불러오기

--- frontend/src/pages/ReportPage.tsx
+++ frontend/src/pages/ReportPage.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import ReportCategory from '@/components/report-page/category/ReportCategory';
 import mockData from '@/components/report-page/mock';
@@ -8,13 +8,25 @@ import ReportProvider from '@/components/report-page/ReportProvider';
 
 import { type CurrencyType } from '@/types/currency';
 
+import { mockData as accountBookMock } from '@/stores/mock';
+import { useAccountBookStore } from '@/stores/useAccountBookStore';
+
 const ReportPage = () => {
+  const accountBook = useAccountBookStore((state) => state.accountBook);
   const categoryData = mockData.compareByCategory;
   const myselfData = mockData.compareWithLastMonth;
   const [currencyType, setCurrencyType] = useState<CurrencyType>('LOCAL'); // @TODO: 드롭다운 추가 예정
 
+  // 추후 삭제될 예정
+  useEffect(() => {
+    if (!accountBook) {
+      console.log('Setting mock account book data');
+      useAccountBookStore.getState().setAccountBook(accountBookMock);
+    }
+  }, [accountBook]);
+
   return (
-    <div className="flex p-3 gap-3.5">
+    <div className="flex gap-3.5 p-3">
       <ReportProvider currencyType={currencyType} onCurrencyTypeChange={setCurrencyType}>
         <div className="flex flex-col justify-between" >
           <ReportMonthly />

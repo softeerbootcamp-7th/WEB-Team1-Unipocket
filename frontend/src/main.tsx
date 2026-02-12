246d49a0493efc8c880ecc1053cd3e6c122ed2e8
Mingyu Seong <minngyuseong@gmail.com>
2026-02-12 14:14:37 +0900

    chore: main.tsx import문 절대경로화

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,11 @@
-import './styles/index.css';
+import '@/styles/index.css';
 
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { createRouter, RouterProvider } from '@tanstack/react-router';
 
-import { routeTree } from './routeTree.gen';
+import { routeTree } from '@/routeTree.gen';
 
 export const queryClient = new QueryClient({
   defaultOptions: {

ad8fe8baac27ff57807c3fcffd139ff5b015edcc
Mingyu Seong <minngyuseong@gmail.com>
2026-02-13 18:47:28 +0900

    chore: ImportToFolderBar 디자인 반영

--- frontend/src/components/data-table/ImportToFolderBar.tsx
+++ frontend/src/components/data-table/ImportToFolderBar.tsx
@@ -1,56 +1,28 @@
-import React from 'react';
-
 import { useDataTable } from '@/components/data-table/context';
 
 import { Icons } from '@/assets';
-import { cn } from '@/lib/utils';
 
 const Divider = () => <div className="bg-line-solid-normal/30 h-6 w-px" />;
-
-const SelectionActionButton = ({
-  children,
-  onClick,
-  className,
-}: React.ComponentProps<'button'>) => {
-  return (
-    <button onClick={onClick} className={cn('label1-normal-medium', className)}>
-      {children}
-    </button>
-  );
-};
 
 const SelectionActionProvider = () => {
   const { table, tableState } = useDataTable();
   const selectedRows = table.getFilteredSelectedRowModel().rows;
 
-  const actionButtons = [
-    { label: '카테고리', onClick: () => console.log('카테고리 변경') },
-    { label: '결제수단', onClick: () => console.log('결제수단 변경') },
-    { label: '여행', onClick: () => console.log('여행 변경') },
-    {
-      label: '삭제',
-      onClick: () => console.log('삭제 실행'),
-      className: 'text-status-negative',
-    },
-  ];
-
   if (!tableState.selectionMode) return null;
+
+  const handleImportToFolder = () => {};
 
   return (
     <div className="bg-background-normal body2-normal-bold rounded-modal-12 shadow-semantic-strong fixed bottom-30 left-1/2 w-125 -translate-x-1/2 truncate px-4 py-2.75">
       <div className="flex items-center gap-4">
         <span className="mr-auto">{selectedRows.length}개 선택됨</span>
-        {actionButtons.map((button, index) => (
-          <React.Fragment key={button.label}>
-            <SelectionActionButton
-              onClick={button.onClick}
-              className={button.className}
-            >
-              {button.label}
-            </SelectionActionButton>
-            {index < actionButtons.length - 1 && <Divider />}
-          </React.Fragment>
-        ))}
+        <button
+          onClick={handleImportToFolder}
+          className="label1-normal-medium text-primary-normal"
+        >
+          현재 폴더에 추가하기
+        </button>
+        <Divider />
         <Icons.Close height={20} width={20} />
       </div>
     </div>

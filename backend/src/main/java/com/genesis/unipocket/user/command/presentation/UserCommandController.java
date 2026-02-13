df363e286e0ec4adf54f4a5a68bad30494687173
blu3fishez <kcw2205@naver.com>
2026-02-13 10:54:40 +0900

    feat(swagger): Swagger 배포 시 Query Command API 를 기능단위로 통합하여 표시

--- backend/src/main/java/com/genesis/unipocket/user/command/presentation/UserCommandController.java
+++ backend/src/main/java/com/genesis/unipocket/user/command/presentation/UserCommandController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "유저 Command API")
+@Tag(name = "유저 기능")
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor

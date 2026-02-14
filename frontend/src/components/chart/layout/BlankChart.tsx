e2b0c9719e135990e04e841b1865c5f9cb7ff408
Kjiw0n <rwd0904@naver.com>
2026-02-14 21:34:42 +0900

    feat: BlanKChart 컴포넌트 추가

---
+++ frontend/src/components/chart/layout/BlankChart.tsx
@@ -0,0 +1,14 @@
+import { useWidgetContext } from '@/components/chart/widget/WidgetContext';
+
+import { Icons } from '@/assets';
+
+const BlankChart = () => {
+  const isEditMode = useWidgetContext().isEditMode;
+  return (
+    <div className="rounded-modal-16 border-line-solid-normal flex h-72 w-67 items-center justify-center border">
+      {isEditMode && <Icons.Add className="text-line-solid-strong size-6" />}
+    </div>
+  );
+};
+
+export default BlankChart;

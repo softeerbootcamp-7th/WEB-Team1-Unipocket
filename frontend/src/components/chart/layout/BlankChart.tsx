23a9f8216d96da5f8d8510759ec2becbee65d099
Kjiw0n <rwd0904@naver.com>
2026-02-15 23:24:50 +0900

    style: 편집모드가 아닐 때 + 아이콘 렌더링하도록 수정

--- frontend/src/components/chart/layout/BlankChart.tsx
+++ frontend/src/components/chart/layout/BlankChart.tsx
@@ -6,7 +6,7 @@ const BlankChart = () => {
   const isEditMode = useWidgetContext().isEditMode;
   return (
     <div className="rounded-modal-16 border-line-solid-normal flex h-72 w-67 items-center justify-center border">
-      {isEditMode && <Icons.Add className="text-line-solid-strong size-6" />}
+      {!isEditMode && <Icons.Add className="text-line-solid-strong size-6" />}
     </div>
   );
 };

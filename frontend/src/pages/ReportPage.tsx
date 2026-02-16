d5baf32da03df84f0db7ed4c50abe3f6a8c0ac47
1jiwoo27 <chloey1227@naver.com>
2026-02-17 01:26:34 +0900

    feat: reportPage 반응형 (min-w-283)

--- frontend/src/pages/ReportPage.tsx
+++ frontend/src/pages/ReportPage.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 
+import Switch from '@/components/common/Switch';
 import ReportCategory from '@/components/report-page/category/ReportCategory';
 import mockData from '@/components/report-page/mock';
 import ReportMonthly from '@/components/report-page/monthly/ReportMonthly';
@@ -8,24 +9,57 @@ import ReportProvider from '@/components/report-page/ReportProvider';
 
 import { type CurrencyType } from '@/types/currency';
 
+import { Icons } from '@/assets';
+
 const ReportPage = () => {
+  const year = new Date().getFullYear();
+  const month = new Date().getMonth() + 1;
   const categoryData = mockData.compareByCategory;
   const myselfData = mockData.compareWithLastMonth;
   const [currencyType, setCurrencyType] = useState<CurrencyType>('LOCAL'); // @TODO: 드롭다운 추가 예정
 
   return (
-    <div className="flex gap-3.5 p-3">
-      <ReportProvider
-        currencyType={currencyType}
-        onCurrencyTypeChange={setCurrencyType}
-      >
-        <div className="flex flex-col justify-between">
-          <ReportMonthly />
-          <ReportMyself data={myselfData} />
+    <div className="flex min-w-283 flex-col gap-9.5 px-30 pt-8">
+      <div className="flex flex-col gap-3">
+        <span className="title2-bold text-label-normal">분석</span>
+        <span className="headline1-medium text-label-alternative">
+          내 월별 소비를 같은 국가 교환학생과 비교하고 전월 대비 현재 지출
+          변화를 살펴봐요
+        </span>
+      </div>
+      <div className="flex min-w-283 flex-col gap-4.75">
+        <div className="flex items-center justify-between">
+          <div className="flex items-center gap-[21.5px]">
+            <span className="title3-medium text-label-normal">
+              {year}년 {month}월
+            </span>
+            <div className="flex gap-3.5">
+              <Icons.ChevronBack className="text-label-neutral size-6" />
+              <Icons.ChevronForward className="text-label-neutral size-6" />
+            </div>
+          </div>
+          <Switch
+            checked={currencyType === 'LOCAL'}
+            onChange={(checked) => setCurrencyType(checked ? 'LOCAL' : 'BASE')}
+          />
         </div>
 
-        <ReportCategory data={categoryData} />
-      </ReportProvider>
+        <div className="flex w-full min-w-283 gap-3.5">
+          <ReportProvider
+            currencyType={currencyType}
+            onCurrencyTypeChange={setCurrencyType}
+          >
+            <div className="flex w-113 flex-col justify-between">
+              <ReportMonthly />
+              <ReportMyself data={myselfData} />
+            </div>
+
+            <div className="flex-1">
+              <ReportCategory data={categoryData} />
+            </div>
+          </ReportProvider>
+        </div>
+      </div>
     </div>
   );
 };

e22d36c068060a9db9994bf090b3858be5d8a69d
Mingyu Seong <minngyuseong@gmail.com>
2026-02-16 21:24:39 +0900

    chore: ExpandableSheet 파일 위치 변경

--- frontend/src/components/layout/ExpandableSheet.tsx
+++ frontend/src/components/layout/ExpandableSheet.tsx
@@ -42,15 +42,17 @@ const ExpandableSheet = ({
         }}
         exit={{ y: '100%' }}
         transition={{ type: 'spring', damping: 25, stiffness: 200 }}
-        className="bg-background-normal absolute bottom-0 flex flex-col overflow-hidden rounded-t-2xl"
+        className="bg-background-normal absolute inset-x-0 bottom-0 flex flex-col overflow-hidden rounded-t-2xl"
       >
         <motion.div
           onPanEnd={handlePanEnd}
-          className="relative flex w-full cursor-grab touch-none justify-center py-4 select-none active:cursor-grabbing"
+          className="relative flex cursor-grab touch-none justify-center py-4 select-none active:cursor-grabbing"
         >
           <div className="bg-cool-neutral-95 absolute top-3 h-1.5 w-12 shrink-0 rounded-full" />
         </motion.div>
-        <div className="flex-1 overflow-y-auto px-2 pb-4">{children}</div>
+        <div className="flex min-h-0 flex-1 flex-col overflow-hidden px-2 pb-4">
+          {children}
+        </div>
       </motion.div>
     </AnimatePresence>
   );

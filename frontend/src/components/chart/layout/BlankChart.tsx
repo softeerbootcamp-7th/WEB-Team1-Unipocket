57c8e11546319776ea7f5fdd4dea24b43cfa9141
Kjiw0n <rwd0904@naver.com>
2026-02-16 21:41:55 +0900

    feat: BlankChart의 +버튼 누를 시 위젯 편집모드로 전환

--- frontend/src/components/chart/layout/BlankChart.tsx
+++ frontend/src/components/chart/layout/BlankChart.tsx
@@ -3,10 +3,14 @@ import { useWidgetContext } from '@/components/chart/widget/WidgetContext';
 import { Icons } from '@/assets';
 
 const BlankChart = () => {
-  const isEditMode = useWidgetContext().isEditMode;
+  const { isWidgetEditMode, toggleEditMode } = useWidgetContext();
   return (
     <div className="rounded-modal-16 border-line-solid-normal flex h-72 w-67 items-center justify-center border">
-      {!isEditMode && <Icons.Add className="text-line-solid-strong size-6" />}
+      {!isWidgetEditMode && (
+        <button type="button" onClick={toggleEditMode}>
+          <Icons.Add className="text-line-solid-strong size-6" />
+        </button>
+      )}
     </div>
   );
 };

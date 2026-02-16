b0b924b31c123e4e5c524b4c209f7f3e74199b15
1jiwoo27 <chloey1227@naver.com>
2026-02-17 01:34:38 +0900

    style: 컴포넌트 높이 및 패딩 미세 조정

--- frontend/src/components/report-page/category/ReportCategory.tsx
+++ frontend/src/components/report-page/category/ReportCategory.tsx
@@ -23,7 +23,7 @@ const ReportCategory = ({ data }: ReportCategoryProps) => {
 
   return (
     <ReportContainer title="월별 지출 비교">
-      <ReportContent className="h-166.5 w-full gap-7 pb-10.25">
+      <ReportContent className="h-162.5 w-full gap-7">
         <h3 className="heading1-bold text-label-normal">
           나랑 같은 국가의 교환학생보다{' '}
           <span className="text-primary-strong">{category}</span> 소비가 유독{' '}

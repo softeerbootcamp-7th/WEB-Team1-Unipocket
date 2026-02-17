91a5a14a613cee6cb7bb18649adac740216ac1af
Kjiw0n <rwd0904@naver.com>
2026-02-18 03:19:59 +0900

    style: Toaster 컴포넌트 스타일링 및 상태별 아이콘 추가(success, error)

--- frontend/src/components/ui/sonner.tsx
+++ frontend/src/components/ui/sonner.tsx
@@ -1,38 +1,32 @@
-import {
-  CircleCheckIcon,
-  InfoIcon,
-  Loader2Icon,
-  OctagonXIcon,
-  TriangleAlertIcon,
-} from "lucide-react"
-import { useTheme } from "next-themes"
-import { Toaster as Sonner, type ToasterProps } from "sonner"
+import { useTheme } from 'next-themes';
+import { Toaster as Sonner, type ToasterProps } from 'sonner';
+
+import { Icons } from '@/assets';
 
 const Toaster = ({ ...props }: ToasterProps) => {
-  const { theme = "system" } = useTheme()
+  const { theme = 'system' } = useTheme();
 
   return (
     <Sonner
-      theme={theme as ToasterProps["theme"]}
+      theme={theme as ToasterProps['theme']}
       className="toaster group"
       icons={{
-        success: <CircleCheckIcon className="size-4" />,
-        info: <InfoIcon className="size-4" />,
-        warning: <TriangleAlertIcon className="size-4" />,
-        error: <OctagonXIcon className="size-4" />,
-        loading: <Loader2Icon className="size-4 animate-spin" />,
+        success: <Icons.CheckmarkCircle className="size-5.5" />,
+        error: <Icons.AlertCircle className="size-5.5" />,
       }}
-      style={
-        {
-          "--normal-bg": "var(--popover)",
-          "--normal-text": "var(--popover-foreground)",
-          "--normal-border": "var(--border)",
-          "--border-radius": "var(--radius)",
-        } as React.CSSProperties
-      }
+      position="bottom-center"
+      offset={40}
+      toastOptions={{
+        classNames: {
+          toast:
+            'group-[.toaster]:!flex group-[.toaster]:!items-center group-[.toaster]:!gap-2 group-[.toaster]:!box-border group-[.toaster]:!w-[420px] group-[.toaster]:!px-4 group-[.toaster]:!py-[11px] group-[.toaster]:!body2-normal-bold group-[.toaster]:!text-[#EAEAEB] group-[.toaster]:!bg-[#8D8D8E] group-[.toaster]:!border-none group-[.toaster]:!rounded-[12px]',
+          title: 'group-[.toaster]:!px-0.5 group-[.toaster]:!py-[5px]',
+          icon: 'group-[.toast]:!shrink-0 group-[.toaster]:!w-fit group-[.toaster]:!h-fit group-[.toaster]:!m-0',
+        },
+      }}
       {...props}
     />
-  )
-}
+  );
+};
 
-export { Toaster }
+export { Toaster };

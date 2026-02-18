556456c60f2be113587936668ee7bf421b0737ab
Mingyu Seong <minngyuseong@gmail.com>
2026-02-18 21:45:14 +0900

    feat: _app 레이아웃 렌더링 전 전체 가계부 api 불러오기 및 전역상태 초기화 - 등록된 가계부 없을 시 온보딩 페이지로 리다이렉트

--- frontend/src/routes/_app.tsx
+++ frontend/src/routes/_app.tsx
@@ -1,10 +1,29 @@
-import { createFileRoute, Outlet } from '@tanstack/react-router';
+import { createFileRoute, Outlet, redirect } from '@tanstack/react-router';
 
 import Header from '@/components/layout/Header';
 import Menu from '@/components/layout/menu/Menu';
 import { Skeleton } from '@/components/ui/skeleton';
 
+import { accountBooksQueryOptions } from '@/api/account-books/query';
+import { useAccountBookStore } from '@/stores/useAccountBookStore';
+
 export const Route = createFileRoute('/_app')({
+  beforeLoad: async ({ context }) => {
+    const { queryClient } = context;
+    const data = await queryClient.ensureQueryData(accountBooksQueryOptions);
+    const { accountBook, setAccountBook } = useAccountBookStore.getState();
+
+    if (!data || data.length === 0) {
+      throw redirect({
+        to: '/init',
+      });
+    }
+
+    if (!accountBook) {
+      const defaultBook = data.find((book) => book.isMain) || data[0];
+      setAccountBook(defaultBook);
+    }
+  },
   pendingComponent: () => <Skeleton className="h-64" />,
   component: AppLayout,
 });

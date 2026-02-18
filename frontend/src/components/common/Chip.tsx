d597b68d460736878d69f2f68c5e0b257f5913f5
Mingyu Seong <minngyuseong@gmail.com>
2026-02-18 21:36:49 +0900

    refactor: Chip 컴포넌트 Category 의존성 제거 - label, bg, text를 prop으로 부모가 전달하도록 수정

--- frontend/src/components/common/Chip.tsx
+++ frontend/src/components/common/Chip.tsx
@@ -1,32 +1,34 @@
 import clsx from 'clsx';
 
-import { CATEGORY_STYLE, type CategoryType } from '@/types/category';
+import { CATEGORIES } from '@/types/category';
 
 import { Icons } from '@/assets';
 
 interface ChipProps {
-  type: CategoryType | string;
+  label: string;
+  bg?: string;
+  text?: string;
   onRemove?: () => void;
 }
 
-const Chip = ({ type, onRemove }: ChipProps) => {
-  const { bg, text } = CATEGORY_STYLE[type as CategoryType] || {
-    bg: 'bg-label-alternative/10',
-    text: 'text-label-alternative',
-  };
-
+const Chip = ({
+  label,
+  bg = CATEGORIES[0]?.bg,
+  text = CATEGORIES[0]?.text,
+  onRemove,
+}: ChipProps) => {
   return (
     <div
       className={clsx('inline-flex items-center rounded-md px-1.5 py-0.75', bg)}
     >
-      <span className={clsx('caption2-bold', text)}>{type}</span>
+      <span className={clsx('caption2-bold', text)}>{label}</span>
       {onRemove && (
         <button
           onClick={(e) => {
-            e.stopPropagation(); // 클릭 시 인풋 포커스 로직과 겹치지 않도록 방지
+            e.stopPropagation();
             onRemove?.();
           }}
-          onMouseDown={(e) => e.preventDefault()} // DataTableFilter의 toggleOption에서 focus()를 다시 호출할 필요가 없어짐.
+          onMouseDown={(e) => e.preventDefault()}
           className={clsx('cursor-pointer rounded-full p-0.5', text)}
         >
           <Icons.Close className="size-3.5" />

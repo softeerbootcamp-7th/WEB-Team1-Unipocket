50c979c5341cdf6731db5a775a6c87cecba3d14c
Kjiw0n <rwd0904@naver.com>
2026-02-18 22:20:08 +0900

    feat: SnackBar 컴포넌트 구현(status: loading, success, default)

---
+++ frontend/src/components/common/Snackbar.tsx
@@ -0,0 +1,61 @@
+import { clsx } from 'clsx';
+
+import Button from '@/components/common/Button';
+
+import { Icons } from '@/assets';
+
+type SnackbarStatus = 'loading' | 'success' | 'default';
+
+interface SnackbarProps {
+  status?: SnackbarStatus;
+  title: string;
+  description?: string;
+  btnText: string;
+  onAction: () => void;
+}
+
+const Snackbar = ({
+  status = 'default',
+  title,
+  description,
+  btnText,
+  onAction,
+}: SnackbarProps) => {
+  const iconMap = {
+    loading: (
+      <Icons.Loading className="text-inverse-label size-5.5 animate-spin" />
+    ),
+    success: <Icons.CheckmarkCircle className="size-5.5" />,
+    default: null,
+  };
+
+  const isLoading = status === 'loading';
+  const isSuccess = status === 'success';
+  return (
+    <div
+      role="status"
+      className="z-priority rounded-modal-12 fixed bottom-10 left-10 flex w-83.75 max-w-105 items-center justify-between gap-8 bg-[#49494B] px-4 py-2.75"
+    >
+      <div className="flex items-center gap-2">
+        {iconMap[status]}
+        <div className="text-inverse-label flex flex-col items-start justify-center px-0.5">
+          <p className="body2-normal-bold">{title}</p>
+          {description && <p className="label2-regular">{description}</p>}
+        </div>
+      </div>
+      <Button
+        variant="outlined-inverse"
+        className={clsx({
+          'shrink-0': true,
+          'border-line-solid-strong': isSuccess,
+        })}
+        onClick={onAction}
+        disabled={isLoading}
+      >
+        {btnText}
+      </Button>
+    </div>
+  );
+};
+
+export default Snackbar;

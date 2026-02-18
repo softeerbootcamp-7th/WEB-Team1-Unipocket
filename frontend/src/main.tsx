ed013462da2709be6ddd8e27a4f19894aac35f5e
Kjiw0n <rwd0904@naver.com>
2026-02-18 16:52:35 +0900

    feat: TooltipProvider 서비스 최상단(main.tsx)에 추가

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,6 +5,8 @@ import { createRoot } from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { createRouter, RouterProvider } from '@tanstack/react-router';
 
+import { TooltipProvider } from '@/components/ui/tooltip';
+
 import { routeTree } from '@/routeTree.gen';
 
 export const queryClient = new QueryClient({
@@ -31,7 +33,9 @@ declare module '@tanstack/react-router' {
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
+      <TooltipProvider delayDuration={300}>
+        <RouterProvider router={router} />
+      </TooltipProvider>
     </QueryClientProvider>
   </StrictMode>,
 );

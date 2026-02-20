dbb2a2c6c89d41b79e7b0e6f5b97cbece29ac588
Mingyu Seong <minngyuseong@gmail.com>
2026-02-20 22:44:18 +0900

    chore: 프로필 팝오버 컴포넌트 수정 - 로그아웃 안되던 로직 수정 - 카카오 계정 프로필 안보이던 문제 해결 - 이메일이 없는 경우, auth provider 아이콘 이름 옆에 배치 - 온보딩 페이지이면 설정 안보이도록

--- frontend/src/components/common/ProfilePopover.tsx
+++ frontend/src/components/common/ProfilePopover.tsx
@@ -4,7 +4,7 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from '@radix-ui/react-popover';
-import { Link, useNavigate } from '@tanstack/react-router';
+import { Link, useMatches, useNavigate } from '@tanstack/react-router';
 import { clsx } from 'clsx';
 
 import Divider from '@/components/common/Divider';
@@ -17,6 +17,8 @@ const ProfilePopover = () => {
   const { data } = useGetUserQuery();
   const logoutMutation = useLogoutMutation();
   const navigator = useNavigate();
+  const matches = useMatches();
+  const isInitPath = matches.some((match) => match.routeId === '/_app/init');
 
   const isGoogleUser = data.profileImgUrl?.includes('googleusercontent');
   const authProvider = AUTH_PROVIDERS.find(
@@ -26,22 +28,9 @@ const ProfilePopover = () => {
 
   const handleLogout = (e: React.MouseEvent) => {
     e.preventDefault();
-
-    // 기존 로그아웃 API 호출 (임시 주석 처리)
-    // logoutMutation.mutate();
-
-    // 로그인할 때와 동일한 도메인 및 옵션 설정
-    const domain = import.meta.env.PROD
-      ? `domain=${import.meta.env.VITE_COOKIE_DOMAIN}; `
-      : '';
-    const baseOptions = `path=/; ${domain}Secure; SameSite=None; `;
-
-    // max-age=0 (또는 과거의 expires)을 사용하여 쿠키 삭제
-    document.cookie = `access_token=; max-age=0; ${baseOptions}`;
-    document.cookie = `refresh_token=; max-age=0; ${baseOptions}`;
-
-    // 로그아웃 후 로그인 페이지나 메인으로 이동 (필요시 주석 해제하여 사용하세요)
-    navigator({ to: '/login' });
+    logoutMutation.mutate();
+    navigator({ to: '/' });
+    return;
   };
 
   return (
@@ -52,7 +41,6 @@ const ProfilePopover = () => {
           alt="프로필 이미지"
           className="h-8 w-8 cursor-pointer rounded-full object-cover"
           referrerPolicy="no-referrer"
-          crossOrigin="anonymous"
         />
       </PopoverTrigger>
       <PopoverContent
@@ -60,29 +48,43 @@ const ProfilePopover = () => {
         className="mt-2"
         onOpenAutoFocus={(e) => e.preventDefault()}
       >
-        <div className="bg-background-normal shadow-popover rounded-modal-18 flex flex-col gap-5 p-8">
+        <div className="bg-background-normal shadow-popover rounded-modal-18 flex min-w-60 flex-col gap-5 p-8">
           <div className="flex flex-col gap-2">
-            <span className="body2-normal-bold">{data.name}</span>
-            <div className="flex gap-2.5">
-              <AuthIcon
-                className={clsx(
-                  'flex size-4 items-center justify-center rounded p-0.75',
-                  authProvider.bgColor,
-                )}
-              />
-              <span className="label1-normal-medium text-label-alternative">
-                {data.email}
-              </span>
+            <div className="flex items-center gap-2.5">
+              {!data.email && (
+                <AuthIcon
+                  className={clsx(
+                    'flex size-4 items-center justify-center rounded p-0.75',
+                    authProvider.bgColor,
+                  )}
+                />
+              )}
+              <span className="body2-normal-bold">{data.name}</span>
             </div>
+            {data.email && (
+              <div className="flex gap-2.5">
+                <AuthIcon
+                  className={clsx(
+                    'flex size-4 items-center justify-center rounded p-0.75',
+                    authProvider.bgColor,
+                  )}
+                />
+                <span className="label1-normal-medium text-label-alternative">
+                  {data.email}
+                </span>
+              </div>
+            )}
           </div>
           <Divider style={'thin'} />
           <PopoverClose asChild>
-            <Link
-              to={'/setting'}
-              className="body2-normal-medium text-label-alternative"
-            >
-              설정
-            </Link>
+            {!isInitPath && (
+              <Link
+                to={'/setting'}
+                className="body2-normal-medium text-label-alternative"
+              >
+                설정
+              </Link>
+            )}
           </PopoverClose>
           <PopoverClose asChild>
             <button

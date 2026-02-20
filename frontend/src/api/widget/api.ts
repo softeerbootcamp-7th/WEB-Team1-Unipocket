7b55945201713e14f8ef7392d3f53cba2a2e83d6
Kjiw0n <rwd0904@naver.com>
2026-02-20 12:28:54 +0900

    feat: api 호출 함수 및 쿼리 구현

--- frontend/src/api/widget/api.ts
+++ frontend/src/api/widget/api.ts
@@ -1,6 +1,10 @@
 import { customFetch } from '@/api/config/client';
 import { ENDPOINTS } from '@/api/config/endpoint';
-import type { GetWidgetRequest } from '@/api/widget/type';
+import type {
+  GetWidgetRequest,
+  UpdateWidgetLayoutRequest,
+  WidgetLayoutResponse,
+} from '@/api/widget/type';
 
 export const getWidget = <T>({
   accountBookId,
@@ -16,6 +20,30 @@ export const getWidget = <T>({
     endpoint: `${ENDPOINTS.WIDGETS.ACCOUNT_BOOK_DATA(accountBookId)}?${params.toString()}`,
     options: {
       method: 'GET',
+    },
+  });
+};
+
+export const getWidgetLayout = (
+  accountBookId: number | string,
+): Promise<WidgetLayoutResponse> => {
+  return customFetch({
+    endpoint: ENDPOINTS.WIDGETS.ACCOUNT_BOOK_LAYOUT(accountBookId),
+    options: {
+      method: 'GET',
+    },
+  });
+};
+
+export const updateWidgetLayout = (
+  accountBookId: number | string,
+  data: UpdateWidgetLayoutRequest,
+): Promise<WidgetLayoutResponse> => {
+  return customFetch({
+    endpoint: ENDPOINTS.WIDGETS.ACCOUNT_BOOK_LAYOUT(accountBookId),
+    options: {
+      method: 'PUT',
+      body: JSON.stringify(data),
     },
   });
 };

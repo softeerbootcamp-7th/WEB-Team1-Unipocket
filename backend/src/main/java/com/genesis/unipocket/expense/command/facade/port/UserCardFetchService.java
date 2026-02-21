263982c983b68932d16213c62ec201f709f856f9
blu3fishez <kcw2205@naver.com>
2026-02-21 22:17:51 +0900

    refactor(expense): 중복된 facade 일원화
    - common 으로 추출하여 중복성 제거

--- backend/src/main/java/com/genesis/unipocket/expense/command/facade/port/UserCardFetchService.java
+++
@@ -1,7 +0,0 @@
-package com.genesis.unipocket.expense.command.facade.port;
-
-import com.genesis.unipocket.expense.command.facade.port.dto.UserCardInfo;
-
-public interface UserCardFetchService {
-	UserCardInfo getUserCard(Long userCardId);
-}
--- backend/src/main/java/com/genesis/unipocket/expense/query/port/UserCardReadService.java
+++
@@ -1,7 +0,0 @@
-package com.genesis.unipocket.expense.query.port;
-
-import com.genesis.unipocket.expense.query.port.dto.UserCardQueryInfo;
-
-public interface UserCardReadService {
-	UserCardQueryInfo readUserCard(Long userCardId);
-}

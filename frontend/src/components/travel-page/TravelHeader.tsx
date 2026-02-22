64d38abf4f95918eb7f649f41be64306c074e331
Kjiw0n <rwd0904@naver.com>
2026-02-22 19:16:38 +0900

    feat: PageHeader 컴포넌트 분리 및 적용

--- frontend/src/components/travel-page/TravelHeader.tsx
+++ frontend/src/components/travel-page/TravelHeader.tsx
@@ -1,14 +1,12 @@
 import Button from '@/components/common/Button';
+import PageHeader from '@/components/layout/PageHeader';
+
+import { PAGE_TITLE } from '@/constants/message';
 
 const TravelHeader = () => {
   return (
     <div className="flex flex-col justify-center gap-11">
-      <div className="flex flex-col justify-start gap-3">
-        <h1 className="title2-semibold text-label-normal">여행 포켓</h1>
-        <h2 className="headline1-medium text-label-alternative">
-          여행별로 지출을 정리해두는 공간이에요
-        </h2>
-      </div>
+      <PageHeader {...PAGE_TITLE.TRAVEL} />
       <div className="flex gap-4">
         <Button variant="solid" size="md">
           여행 포켓 생성하기

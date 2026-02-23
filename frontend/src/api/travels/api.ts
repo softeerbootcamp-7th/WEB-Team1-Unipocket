c51f683a0184e94771c19d56d961ec564af6cd07
Kjiw0n <rwd0904@naver.com>
2026-02-23 12:33:35 +0900

    chore: travel API fetch 함수 정의

---
+++ frontend/src/api/travels/api.ts
@@ -0,0 +1,116 @@
+import { customFetch } from '@/api/config/client';
+import { ENDPOINTS } from '@/api/config/endpoint';
+import type {
+  CreateTravelRequest,
+  GetImageUrlResponse,
+  GetPresignedUrlRequest,
+  GetPresignedUrlResponse,
+  GetTravelAmountResponse,
+  GetTravelDetailResponse,
+  GetTravelsResponse,
+  PatchTravelRequest,
+  UpdateTravelRequest,
+  UpdateTravelResponse,
+} from '@/api/travels/type';
+
+export const getTravels = (
+  accountBookId: number | string,
+): Promise<GetTravelsResponse> => {
+  return customFetch({
+    endpoint: ENDPOINTS.TRAVELS.BASE(accountBookId),
+    options: { method: 'GET' },
+  });
+};
+
+export const createTravel = (
+  accountBookId: number | string,
+  data: CreateTravelRequest,
+): Promise<void> => {
+  return customFetch({
+    endpoint: ENDPOINTS.TRAVELS.BASE(accountBookId),
+    options: {
+      method: 'POST',
+      body: JSON.stringify(data),
+    },
+  });
+};
+
+export const getTravelDetail = (
+  accountBookId: number | string,
+  travelId: number | string,
+): Promise<GetTravelDetailResponse> => {
+  return customFetch({
+    endpoint: ENDPOINTS.TRAVELS.DETAIL(accountBookId, travelId),
+    options: { method: 'GET' },
+  });
+};
+
+export const updateTravel = (
+  accountBookId: number | string,
+  travelId: number | string,
+  data: UpdateTravelRequest,
+): Promise<UpdateTravelResponse> => {
+  return customFetch({
+    endpoint: ENDPOINTS.TRAVELS.DETAIL(accountBookId, travelId),
+    options: {
+      method: 'PUT',
+      body: JSON.stringify(data),
+    },
+  });
+};
+
+export const patchTravel = (
+  accountBookId: number | string,
+  travelId: number | string,
+  data: PatchTravelRequest,
+): Promise<void> => {
+  return customFetch({
+    endpoint: ENDPOINTS.TRAVELS.DETAIL(accountBookId, travelId),
+    options: {
+      method: 'PATCH',
+      body: JSON.stringify(data),
+    },
+  });
+};
+
+export const deleteTravel = (
+  accountBookId: number | string,
+  travelId: number | string,
+): Promise<void> => {
+  return customFetch({
+    endpoint: ENDPOINTS.TRAVELS.DETAIL(accountBookId, travelId),
+    options: { method: 'DELETE' },
+  });
+};
+
+export const getTravelPresignedUrl = (
+  accountBookId: number | string,
+  data: GetPresignedUrlRequest,
+): Promise<GetPresignedUrlResponse> => {
+  return customFetch({
+    endpoint: ENDPOINTS.TRAVELS.PRESIGNED_URL(accountBookId),
+    options: {
+      method: 'POST',
+      body: JSON.stringify(data),
+    },
+  });
+};
+
+export const getTravelImageUrl = (
+  accountBookId: number | string,
+): Promise<GetImageUrlResponse> => {
+  return customFetch({
+    endpoint: ENDPOINTS.TRAVELS.IMAGE_URL(accountBookId),
+    options: { method: 'GET' },
+  });
+};
+
+export const getTravelAmount = (
+  accountBookId: number | string,
+  travelId: number | string,
+): Promise<GetTravelAmountResponse> => {
+  return customFetch({
+    endpoint: ENDPOINTS.TRAVELS.AMOUNT(accountBookId, travelId),
+    options: { method: 'GET' },
+  });
+};

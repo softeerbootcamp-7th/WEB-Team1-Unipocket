72ec2fee7fb257896ab3ddca9845ff173926416d
Mingyu Seong <minngyuseong@gmail.com>
2026-02-25 11:43:28 +0900

    fix: 그룹화 행은 인덴트 주기, 페이지네이션시 체크 내역 취소

--- frontend/src/components/data-table/DataTablePagination.tsx
+++ frontend/src/components/data-table/DataTablePagination.tsx
@@ -1,5 +1,7 @@
 import type { ReactNode } from 'react';
 
+import { useDataTable } from '@/components/data-table/context';
+
 import { Icons } from '@/assets';
 
 interface PaginationButtonProps {
@@ -40,15 +42,21 @@ export const DataTablePagination = ({
 }: DataTablePaginationProps) => {
   const canGoPrev = page > 0;
   const canGoNext = page < totalPages - 1;
+  const { dispatch } = useDataTable();
 
   if (totalPages <= 1) return null;
+
+  const handlePageChange = (newPage: number) => {
+    dispatch({ type: 'SET_ROW_SELECTION', payload: {} });
+    onPageChange(newPage);
+  };
 
   return (
     <div className="bg-background-normal relative flex items-center justify-center pt-1 pb-4">
       <div className="from-background-normal to-background-normal/0 pointer-events-none absolute -top-8 left-0 h-8 w-full bg-linear-to-t" />
 
       <PaginationButton
-        onClick={() => onPageChange(page - 1)}
+        onClick={() => handlePageChange(page - 1)} // 핸들러 변경
         disabled={!canGoPrev}
         ariaLabel="이전 페이지"
       >
@@ -60,7 +68,7 @@ export const DataTablePagination = ({
       </div>
 
       <PaginationButton
-        onClick={() => onPageChange(page + 1)}
+        onClick={() => handlePageChange(page + 1)} // 핸들러 변경
         disabled={!canGoNext}
         ariaLabel="다음 페이지"
       >

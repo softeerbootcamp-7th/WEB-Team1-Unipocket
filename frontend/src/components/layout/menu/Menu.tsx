da4f78b43d70a588f03910374484157635e5ed54
Mingyu Seong <minngyuseong@gmail.com>
2026-02-25 23:25:22 +0900

    chore: 아이콘 클릭시 현재화면이면 key값 변경해서 outlet 리렌더링

--- frontend/src/components/layout/menu/Menu.tsx
+++ frontend/src/components/layout/menu/Menu.tsx
@@ -1,8 +1,9 @@
-import { Link, useMatchRoute } from '@tanstack/react-router';
+import { Link, useMatchRoute, useRouter } from '@tanstack/react-router';
 
 import MenuItem from '@/components/layout/menu/MenuItem';
 
 import { Icons } from '@/assets';
+import { useRefreshStore } from '@/stores/refreshStore';
 
 const menuItems = [
   { to: '/home', Icon: Icons.Home, label: '홈' },
@@ -12,24 +13,50 @@ const menuItems = [
 
 const Menu = () => {
   const matchRoute = useMatchRoute();
+  const router = useRouter();
+  const triggerRefresh = useRefreshStore((s) => s.triggerRefresh);
+
   const isInitPath = !!matchRoute({ to: '/init' });
 
   return (
     <nav className="border-line-normal-normal bg-background-normal sticky top-0 bottom-0 left-0 flex w-16 flex-col items-center gap-9 border-r px-4 py-3">
-      <Link to="/home">
+      <Link
+        to="/home"
+        onClick={(e) => {
+          if (matchRoute({ to: '/home' })) {
+            e.preventDefault();
+            router.invalidate();
+            triggerRefresh();
+          }
+        }}
+      >
         <Icons.Logo className="size-8 cursor-pointer" />
       </Link>
       {!isInitPath && (
         <div className="flex flex-col gap-6">
-          {menuItems.map(({ to, Icon, label }) => (
-            <Link key={to} to={to}>
-              <MenuItem
-                logo={<Icon className="size-5" />}
-                label={label}
-                isActive={!!matchRoute({ to, fuzzy: true })}
-              />
-            </Link>
-          ))}
+          {menuItems.map(({ to, Icon, label }) => {
+            const isActive = !!matchRoute({ to, fuzzy: true });
+
+            return (
+              <Link
+                key={to}
+                to={to}
+                onClick={(e) => {
+                  if (isActive) {
+                    e.preventDefault();
+                    router.invalidate();
+                    triggerRefresh();
+                  }
+                }}
+              >
+                <MenuItem
+                  logo={<Icon className="size-5" />}
+                  label={label}
+                  isActive={isActive}
+                />
+              </Link>
+            );
+          })}
         </div>
       )}
     </nav>

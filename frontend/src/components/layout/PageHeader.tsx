b7f6694e21dd6a6584a0912d25978dd042c41f09
Kjiw0n <rwd0904@naver.com>
2026-02-26 13:58:32 +0900

    refactor: 여행 페이지에 info 아이콘 추가

--- frontend/src/components/layout/PageHeader.tsx
+++ frontend/src/components/layout/PageHeader.tsx
@@ -1,12 +1,24 @@
+import Tooltip from '@/components/common/Tooltip';
+
+import { Icons } from '@/assets';
+
 interface PageHeaderProps {
   title: string;
   subtitle: string;
+  tooltipContent?: string;
 }
 
-const PageHeader = ({ title, subtitle }: PageHeaderProps) => {
+const PageHeader = ({ title, subtitle, tooltipContent }: PageHeaderProps) => {
   return (
     <div className="flex flex-col justify-start gap-2.5">
-      <h1 className="title2-semibold text-label-normal">{title}</h1>
+      <div className="flex items-center justify-start gap-2.5">
+        <h1 className="title2-semibold text-label-normal">{title}</h1>
+        {tooltipContent && (
+          <Tooltip content={tooltipContent} side="bottom">
+            <Icons.Information className="size-6 cursor-pointer" />
+          </Tooltip>
+        )}
+      </div>
       <h2 className="headline1-medium text-label-alternative">{subtitle}</h2>
     </div>
   );
